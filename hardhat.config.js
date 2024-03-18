4423cd66c0cb21504cc2119d349b0a462a9684d9
Ekarika Nsemeke <hamatthan@gmail.com>
2024-03-19 00:34:10 +0100

    verified the contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const { URL, PRIVATE_KEY } = process.env;
+const { URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 module.exports = {
   solidity: "0.8.20",
   networks: {
@@ -22,5 +22,8 @@ module.exports = {
       url: "https://ethereum-goerli.publicnode.com",
       accounts: [process.env.PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
